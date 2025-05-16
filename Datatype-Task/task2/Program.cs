55bc6869380c148f6acd069d4e4ba78d0dbf313f
steffil13 <ashinsteffil13@gmail.com>
2025-05-16 15:24:22 +0530

    Added loop assignment and datatype assignment re uploaded

---
+++ Datatype-Task/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            namespace task3
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //get the distamce from user
+                Console.Write("Enter the distance:");
+                //assign the variable for distance
+                int distance = Convert.ToInt32(Console.ReadLine());
+                //get the time and speed
+                Console.Write("Enter the time:");
+                int time = Convert.ToInt32(Console.ReadLine());
+                int speed = distance / time;
+                //display kmph and mph
+                Console.WriteLine($"The speed in killometer per hour is: {speed}");
+                Console.WriteLine($"The speed in miles per hour is: {speed}");
+            }
+        }
+    }
+
+}
+    }
+}
